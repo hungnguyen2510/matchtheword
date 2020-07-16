ecbe0ecdf5ea0864bb17f15c52e3d1516a4b19d3
hungnguyen2510 <htmm2510@gmail.com>
2020-07-16 14:01:09 +0700

    add room, done room

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let app = require("express")();
 let http = require("http").Server(app);
 let io = require("socket.io")(http);
+const rooms = {};
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -11,12 +12,14 @@ http.listen(3000, () => {
 });
 
 io.on("connection", (socket) => {
+  console.log("connection");
   socket.on("disconnect", () => {
     console.log("disconnect");
   });
 
   socket.on("chat-message", (data) => {
-    socket.broadcast.emit("chat-message", data);
+    // socket.broadcast.emit("chat-message", data);
+    console.log(data);
   });
 
   socket.on("typing", (data) => {
@@ -24,13 +27,32 @@ io.on("connection", (socket) => {
     // socket.broadcast.emit("typing", data);
   });
 
+  socket.on("leave", (data) => {
+    console.log(data);
+    rooms[data.room] = rooms[data.room].filter((user) => user.id !== data.id);
+    socket.broadcast.emit("leave", rooms[data.room]);
+  });
+
   socket.on("stopTyping", (data) => {
     socket.broadcast.emit("stopTyping", data);
   });
 
+  socket.on("create_room", (data) => {
+    socket.join(data.room);
+    console.log("create_room: ", data);
+    // io.to(data.room).emit("create_room", data);
+  });
+
   socket.on("joined", (data) => {
-    io.emit("connections", 1);
-    io.emit("joined", data);
-    console.log(data);
+    if (!rooms[data.room]) {
+      rooms[data.room] = [];
+    }
+    rooms[data.room].push(data);
+    console.log("joined: ", data);
+    socket.join(data.room);
+    // io.emit("connections", 1);
+    io.in(data.room).emit("joined", rooms[data.room]);
+    // io.to(data.room).emit("connections", 1);
+    // io.to(data.room).emit("joined", data);
   });
 });
